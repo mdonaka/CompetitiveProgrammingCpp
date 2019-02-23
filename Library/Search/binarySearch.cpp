967f32efc9ec16ccfe537eac7ecb84f45757ab69
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2019-02-24 03:52:51 +0900

    fix binary search

--- Library/Search/binarySearch.cpp
+++ Library/Search/binarySearch.cpp
@@ -1,6 +1,10 @@
+/**
+* 整数値 単調増加
+* 単調減少 -> 真偽逆転 ng = ok
+*/
 ll ok = 1e18;
 ll ng = -1;
-while (std::abs(ok - ng) > 1) {
+while (ok - ng > 1) {
 	ll mid = (ok + ng) / 2;
 	ll isOk = true;
 	/*** 判定 ***/
