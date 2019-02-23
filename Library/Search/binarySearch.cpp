e46c94010421bbbefe9c4ef60029dd2c9ab2d959
mdonaka <tmdonkt042+eg@gmail.com>
2019-02-24 03:22:22 +0900

    add binary search

---
+++ Library/Search/binarySearch.cpp
@@ -0,0 +1,11 @@
+ll ok = 1e18;
+ll ng = -1;
+while (std::abs(ok - ng) > 1) {
+	ll mid = (ok + ng) / 2;
+	ll isOk = true;
+	/*** 判定 ***/
+    
+	/************/
+	if (isOk) { ok = mid; }
+	else { ng = mid; }
+}
