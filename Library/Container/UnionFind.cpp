a15863dae3bb61ff19fe1ffe7d1d427cc498b411
mdonaka <tmdonkt042+eg@gmail.com>
2019-03-05 18:38:21 +0900

    update UnionFind

--- Library/Container/UnionFind.cpp
+++ Library/Container/UnionFind.cpp
@@ -3,14 +3,9 @@
  *	nodeの深さ，木のサイズを取得可能
  */
 class UnionFind {
-public:
 	std::vector<ll> m_root;
 	std::vector<ll> m_depth;
 	std::vector<ll> m_size;
-	ll root(ll x) {
-		if (m_root[x] == x) return x;
-		return m_root[x] = root(m_root[x]);
-	}
 public:
 	UnionFind(ll size_) : m_root(size_), m_depth(size_, 0), m_size(size_, 1) {
 		std::iota(m_root.begin(), m_root.end(), 0);
@@ -27,6 +22,10 @@ public:
 	}
 	bool isSame(ll x, ll y) {
 		return root(x) == root(y);
+	}
+	ll root(ll x) {
+		if (m_root[x] == x) return x;
+		return m_root[x] = root(m_root[x]);
 	}
 	ll size(ll x) {
 		if (m_root[x] == x) return m_size[x];
