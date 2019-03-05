fe02fc88afe9a33bf67fd1ca8fd1fd383eecae4e
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2019-03-05 17:07:46 +0900

    update UnionFind

--- Library/Container/UnionFind.cpp
+++ Library/Container/UnionFind.cpp
@@ -1,23 +1,35 @@
+/**
+ *	UnionFind木を構成する
+ *	nodeの深さ，木のサイズを取得可能
+ */
 class UnionFind {
+public:
 	std::vector<ll> m_root;
+	std::vector<ll> m_depth;
+	std::vector<ll> m_size;
 	ll root(ll x) {
-		if (m_root[x] == x) { return x; }
+		if (m_root[x] == x) return x;
 		return m_root[x] = root(m_root[x]);
 	}
 public:
-	UnionFind() = delete;
-	UnionFind(ll n) :m_root(n) {
+	UnionFind(ll size_) : m_root(size_), m_depth(size_, 0), m_size(size_, 1) {
 		std::iota(m_root.begin(), m_root.end(), 0);
 	}
-
-	bool isSame(ll x, ll y) {
-		return m_root[x] == m_root[y];
+	void unite(ll x, ll y) {
+		x = root(x);
+		y = root(y);
+		if (x == y) return;
+		ll t = size(x) + size(y);
+		m_size[x] = m_size[y] = t;
+		if (m_depth[x] < m_depth[y]) m_root[x] = y;
+		else m_root[y] = x;
+		if (m_depth[x] == m_depth[y]) ++m_depth[x];
 	}
-
-	void merge(ll x, ll y) {
-		ll rx = root(x);
-		ll ry = root(y);
-		if (rx == ry) return;
-		m_root[rx] = ry;
+	bool isSame(ll x, ll y) {
+		return root(x) == root(y);
+	}
+	ll size(ll x) {
+		if (m_root[x] == x) return m_size[x];
+		return size(m_root[x] = root(m_root[x]));
 	}
 };
