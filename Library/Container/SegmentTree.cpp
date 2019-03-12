c969f42f285549aec953cc8bb2ea82d921374425
mdonaka <tmdonkt042+eg@gmail.com>
2019-03-12 19:39:01 +0900

    update SegmentTree

--- Library/Container/SegmentTree.cpp
+++ Library/Container/SegmentTree.cpp
@@ -1,8 +1,14 @@
+/**
+ *	セグメント木を構成する
+ *	2methodの変更により調整
+ */
 class SegmentTree {
 private:
+	const ll initialValue = (1LL << 31) - 1;
+	const ll ignoreValue = (1LL << 31) - 1;
+
 	const ll m_size;
-	const ll m_initialValue;
-	std::vector<ll> m_tree;
+	std::vector<ll> m_node;
 
 	ll calcSize(ll n) {
 		ll size = 1;
@@ -11,28 +17,43 @@ private:
 		}
 		return size;
 	}
-public:
-	SegmentTree(ll n, ll val) :
-		m_size(calcSize(n)),
-		m_initialValue(val),
-		m_tree(m_size * 2 - 1, m_initialValue) {
+
+	void update(ll k, ll x) {
+		m_node[k] = x;
 	}
 
-	void update(ll itr, ll val) {
+	ll merge(ll xl, ll xr) {
+		return std::min(xl, xr);
+	}
+public:
+	SegmentTree(ll n) :
+		m_size(calcSize(n)),
+		m_node(m_size * 2 - 1, initialValue) {
+	}
+
+	void add(ll itr, ll val) {
 		ll i = itr + m_size - 1;
-		m_tree[i] += val;
+		update(i, val);
 
 		while (i > 0) {
 			i = (i - 1) / 2;
-			m_tree[i] = m_tree[i * 2 + 1] + m_tree[i * 2 + 2];
+			m_node[i] = merge(m_node[i * 2 + 1], m_node[i * 2 + 2]);
 		}
+		/**
+		cout << "-- tree -- " << endl;
+		REP(i, 2 * m_size - 1) {
+		cout << m_node[i] << endl;
+		}
+		/*//**/
 	}
 
 	ll query(ll a, ll b) { return query(a, b + 1, 0, 0, m_size); }
 	ll query(ll a, ll b, ll k, ll l, ll r) {
-		if (r <= a || b <= l) { return m_initialValue; }
-		if (a <= l && r <= b) { return m_tree[k]; }
-		return query(a, b, 2 * k + 1, l, (l + r) / 2) +
-			query(a, b, 2 * k + 2, (l + r) / 2, r);
+		if (r <= a || b <= l) { return ignoreValue; }
+		if (a <= l && r <= b) { return m_node[k]; }
+		return merge(
+			query(a, b, 2 * k + 1, l, (l + r) / 2),
+			query(a, b, 2 * k + 2, (l + r) / 2, r)
+		);
 	}
 };
