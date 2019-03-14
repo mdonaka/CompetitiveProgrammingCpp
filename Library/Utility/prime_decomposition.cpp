2a2cbb880c2c08d1463423d9da8cf2ea50af4ecc
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2019-03-15 05:41:01 +0900

    update prime decomposition more faster

--- Library/Utility/prime_decomposition.cpp
+++ Library/Utility/prime_decomposition.cpp
@@ -1,9 +1,9 @@
 /**
- *  素因数分解をし，全ての素因数を返す
- */
-multiset<ll> prime_decomposition(ll n) {
+*  素因数分解をし，全ての素因数を返す
+*/
+std::unordered_multiset<ll> prime_decomposition(ll n) {
 	ll i = 2;
-	multiset<ll> table{};
+	std::unordered_multiset<ll> table{};
 	while (i * i <= n) {
 		while (n % i == 0) {
 			n /= i;
