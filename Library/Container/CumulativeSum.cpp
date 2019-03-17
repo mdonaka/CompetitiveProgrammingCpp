ac354cf10debfe5acca9669001632f8a44d1e11f
mdonaka <tmdonkt042+eg@gmail.com>
2019-03-18 02:14:59 +0900

    add cumulative sum

---
+++ Library/Container/CumulativeSum.cpp
@@ -0,0 +1,22 @@
+/**
+ *  累積和を生成
+ *  閉区間([l,r])における値を取得
+ */
+class CumulativeSum {
+	std::vector<ll> sumList;
+public:
+
+	CumulativeSum() = delete;
+	CumulativeSum(const std::vector<ll>& v) :sumList(v.size() + 1) {
+		for (ll i = 0; i < v.size(); ++i) {
+			sumList[i + 1] = sumList[i] + v[i];
+		}
+	}
+	ll get(ll n) {
+		return sumList[n + 1];
+	}
+	ll get(ll l, ll r) {
+        if(l < r){ return 0LL; }
+		return sumList[r + 1] - sumList[l];
+	}
+};
