8b8cc9bc4bc3671884254f2a796b4060f76ce148
mdonaka <tmdonkt042+eg@gmail.com>
2019-03-18 03:47:30 +0900

    update cumulative sum more safely

--- Library/Container/CumulativeSum.cpp
+++ Library/Container/CumulativeSum.cpp
@@ -3,12 +3,13 @@
  *  閉区間([l,r])における値を取得
  */
 class CumulativeSum {
+	const ll size;
 	std::vector<ll> sumList;
 public:
 
 	CumulativeSum() = delete;
-	CumulativeSum(const std::vector<ll>& v) :sumList(v.size() + 1) {
-		for (ll i = 0; i < v.size(); ++i) {
+	CumulativeSum(const std::vector<ll>& v) :size(v.size()), sumList(size + 1) {
+		for (ll i = 0; i < size; ++i) {
 			sumList[i + 1] = sumList[i] + v[i];
 		}
 	}
@@ -16,7 +17,8 @@ public:
 		return sumList[n + 1];
 	}
 	ll get(ll l, ll r) {
-        if(r < l){ return 0LL; }
+		if (r < l) { return 0LL; }
+		l = std::max(l, 0LL); r = std::min(r, size - 1);
 		return sumList[r + 1] - sumList[l];
 	}
 };
