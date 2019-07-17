b50ab6d6078a4c32ea7ec34041337cea56e1c08c
mdonaka <tmdonkt042+eg@gmail.com>
2019-07-17 18:24:03 +0900

    add warshalFloyd

--- Library/Graph/dijkstra.cpp
+++ Library/Graph/dijkstra.cpp
@@ -1,8 +1,8 @@
 /**
- *  ダイクストラにより任意のnodeから全nodeへの最初コストを得る
+ *  ダイクストラにより任意のnodeから全nodeへの最小コストを得る
  *  O((E+V)logV)
  */
-VL dijkstra(const unordered_multimap<ll, PAIR> graph, ll n, ll begin) {
+VL dijkstra(const unordered_multimap<ll, PAIR>& graph, ll n, ll begin) {
 	VL cost(n, 1e15);
 	cost[begin] = 0;
 	priority_queue<PAIR, vector<PAIR>, std::greater<PAIR>> q;
