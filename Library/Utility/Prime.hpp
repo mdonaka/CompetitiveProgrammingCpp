ef00ebd677449e469b6b3a1b3796e0638b66775a
mdonaka <tmdonkt042+eg@gmail.com>
2021-10-10 11:00:15 +0900

    add prime factorization

--- Library/Utility/Prime.hpp
+++ Library/Utility/Prime.hpp
@@ -2,9 +2,14 @@
 
 #include <vector>
 #include <deque>
+#include <unordered_map>
 
 class Prime {
+	const int n;
+	const std::deque<int> p_list;
+
 public:
+
 	static inline auto linearSieve(int n) {
 		std::deque<int> p_list;
 		std::vector<int> lpf(n + 1);
@@ -20,4 +25,20 @@ public:
 		}
 		return std::tuple{ p_list,lpf };
 	}
+
+	Prime(int n) :n(n), p_list(std::get<0>(linearSieve(n))) {}
+
+	/* n‚Ísqrt(max(x))‚ ‚ê‚Î\•ª‚È‚Ì‚Å‹C‚ğ•t‚¯‚é */
+	auto factorization(int x) const {
+		std::unordered_map<int, int> table;
+		for (const auto& p : p_list) {
+			while (x % p == 0) {
+				table[p]++;
+				x /= p;
+			}
+		}
+		if (x > 1) { table[x]++; }
+		return table;
+	}
+
 };
