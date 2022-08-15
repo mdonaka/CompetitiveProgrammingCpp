1dae7b914ea15bfbcf454d5f8befeb8c81c3b3ba
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-08-15 21:11:54 +0900

    add edge range of HLD

--- Library/Graph/Tree/HLD.hpp
+++ Library/Graph/Tree/HLD.hpp
@@ -15,6 +15,8 @@ class HLD {
     const std::vector<node_t> m_height;
     const std::vector<std::pair<node_t, node_t>> m_root_par;
     const std::vector<node_t> m_ids;
+    const std::vector<node_t> m_order;
+    const std::vector<node_t> m_edge_ids;
 
     static auto constructGraph(node_t n, const Graph_f& tree) {
         std::deque<std::pair<node_t, node_t>> order;
@@ -109,6 +111,33 @@ class HLD {
         return ids;
     }
 
+    auto constructOrder()const {
+        std::vector<node_t> order(m_n);
+        for(int i = 0; i < m_n; ++i) { order[m_ids[i]] = i; }
+        return order;
+    }
+    /*
+     * •Ó‚ðnode‚Æ‚µ‚ÄŠg’£‚µ‚½ê‡‚Ì•Ónode‚¾‚¯ID‚ðU‚é
+     * (1) - (2)
+     * (1) - (e) - (2)
+     * [-1, -1, 0]
+     */
+    auto constructEdgeIds() const {
+        node_t edge_size = (m_n >> 1);
+        std::vector<node_t> edge_ids(m_n, -1);
+        node_t val = 0;
+        std::stack<node_t> stk;
+        stk.emplace(0);
+        while(!stk.empty()) {
+            auto f = stk.top();
+            stk.pop();
+            if(f > edge_size) { edge_ids[f] = val; ++val; }
+            if(m_tree.find(f) == m_tree.end()) { continue; }
+            for(const auto& t : m_tree.at(f)) { stk.emplace(t); }
+        }
+        return edge_ids;
+    }
+
 public:
 
     HLD(node_t n, const Graph_f& tree) :
@@ -116,15 +145,14 @@ public:
         m_tree(constructGraph(n, tree)),
         m_root_par(constructRootPar(n, m_tree)),
         m_height(constructHeight(n, m_tree)),
-        m_ids(constructIds()) {
-    }
-    auto hld_order_vec()const {
-        std::vector<node_t> order(m_n);
-        for(int i = 0; i < m_n; ++i) { order[m_ids[i]] = i; }
-        return order;
+        m_ids(constructIds()),
+        m_order(constructOrder()),
+        m_edge_ids(constructEdgeIds()) {
     }
 
-    auto get_id(node_t i)const { return m_ids[i]; }
+    auto getId(node_t i)const { return m_ids[i]; }
+    auto getEdgeId(node_t i)const { return m_edge_ids[i]; }
+    auto getOrder(node_t i)const { return m_order[i]; }
 
     auto lca(node_t f, node_t t)const {
         do {
@@ -144,6 +172,30 @@ public:
             auto l = std::min(m_ids[f], m_ids[t]);
             auto r = std::max(m_ids[f], m_ids[t]);
             ret.emplace_back(l, r);
+        };
+        do {
+            auto [fr, fp] = m_root_par[f];
+            auto [tr, tp] = m_root_par[t];
+            if(fr == tr) { add(f, t); break; }
+            auto fph = (fp > -1) ? m_height[fp] : -1;
+            auto tph = (tp > -1) ? m_height[tp] : -1;
+            if(fph < tph) { add(t, tr); t = tp; } else { add(f, fr); f = fp; }
+        } while(true);
+        return ret;
+    }
+
+    auto rangeEdge(node_t f, node_t t)const {
+        node_t edge_size = (m_n >> 1);
+        std::deque<std::pair<node_t, node_t>> ret;
+        auto add = [&](node_t f, node_t t) {
+            auto l = std::min(m_ids[f], m_ids[t]);
+            auto r = std::max(m_ids[f], m_ids[t]);
+            if(m_order[l] <= edge_size) { ++l; }
+            if(m_order[r] <= edge_size) { --r; }
+            if(l > r) { return; }
+            auto edge_l = m_edge_ids[m_order[l]];
+            auto edge_r = m_edge_ids[m_order[r]];
+            ret.emplace_back(edge_l, edge_r);
         };
         do {
             auto [fr, fp] = m_root_par[f];
