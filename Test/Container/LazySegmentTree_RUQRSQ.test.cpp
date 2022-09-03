b596268e5ed64e9a5d82f9cbd02c83ffbfac1b9a
mdonaka <tmdonkt042+eg@gmail.com>
2022-09-03 18:54:43 +0900

    remove lazySegmentTree comment

---
+++ Test/Container/LazySegmentTree_RUQRSQ.test.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+// #include "./../../Library/Container/LazySegmentTree.hpp"
+
+using ll = long long;
+using std::cout;
+using std::cin;
+constexpr char endl = '\n';
+
+signed main() {
+
+}
