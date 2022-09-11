7ee98c1073e71b37211a4d35da6de789402b0829
mdonaka <tmdonkt042+eg@gmail.com>
2022-09-11 19:24:51 +0900

    fix SuffixArray test to fast cin

--- Test/String/SuffixArray.test.cpp
+++ Test/String/SuffixArray.test.cpp
@@ -8,7 +8,7 @@ using ll = long long;
 using std::cout;
 using std::cin;
 constexpr char endl = '\n';
-
+struct Preprocessing { Preprocessing() { std::cin.tie(0); std::ios::sync_with_stdio(0); }; }_Preprocessing;
 
 signed main() {
     std::string s;
