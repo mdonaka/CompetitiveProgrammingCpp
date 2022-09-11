a6511e2426bcc34bd66ea70a3649574182ce185e
mdonaka <tmdonkt042+eg@gmail.com>
2022-09-11 12:04:57 +0900

    update main_srm

--- Main/main_srm.cpp
+++ Main/main_srm.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::cout;
+using std::endl;
 using std::vector;
-const char endl = '\n';
 
 struct Preprocessing { Preprocessing() { std::cin.tie(0); std::ios::sync_with_stdio(0); }; }_Preprocessing;
-template<class T> std::istream& operator >> (std::istream& is, std::vector<T>& vec) { for(T& x : vec) is >> x; return is; }
 
 //=============================================================================================
 
