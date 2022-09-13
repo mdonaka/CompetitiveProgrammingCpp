d514134c2287205aa6c19c54ac77f491d2acb9e5
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-09-14 04:40:09 +0900

    update euclideanAlgorithm to use right value

---
+++ Test/Algorithms/EuclideanAlgorithm_gcd.test.cpp
@@ -0,0 +1,15 @@
+#define IGNORE
+// TODO: いい問題を探す
+// #define PROBLEM ""
+
+#include <iostream>
+
+using ll = long long;
+using std::cout;
+using std::cin;
+constexpr char endl = '\n';
+struct Preprocessing { Preprocessing() { std::cin.tie(0); std::ios::sync_with_stdio(0); }; }_Preprocessing;
+
+signed main() {
+
+}
