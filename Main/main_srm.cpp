9fb73a284f9ca64787f0718fb5684b8416fc82c3
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2022-09-18 00:12:46 +0900

    clean main

--- Main/main_srm.cpp
+++ Main/main_srm.cpp
@@ -4,7 +4,9 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 using std::vector;
+using std::string;
 
 struct Preprocessing { Preprocessing() { std::cin.tie(0); std::ios::sync_with_stdio(0); }; }_Preprocessing;
 
