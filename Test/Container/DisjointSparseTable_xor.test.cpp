192f3b1758a496aa44a0f0769a501d2d11db08dd
mdonaka <tmdonkt042+eg@gmail.com>
2023-01-14 04:15:05 +0900

    add DisjointsparseTable xor test

---
+++ Test/Container/DisjointSparseTable_xor.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1456"
+
+#include <iostream>
+#include <unordered_set>
+
+#include "./../../Library/Container/DisjointSparseTable.hpp"
+
+using ll = long long;
+using std::cout;
+using std::cin;
+constexpr char endl = '\n';
+
+struct F { auto operator()(ll x, ll y) { return x ^ y; } };
+using SG = SemiGroup<ll, F>;
+
+signed main() {
+    ll n, k;
+    cin >> n >> k;
+
+    std::vector<ll> a; a.reserve(n);
+    for(int _ = 0; _ < n; ++_) {
+        ll x; cin >> x;
+        a.emplace_back(x);
+    }
+
+    auto dst = DisjointSparseTable<SG>(n, a);
+    std::unordered_set<ll> st;
+    for(int i = 0; i < n; ++i) {
+        st.emplace(dst.get(0, i) ^ k);
+    }
+
+    if(st.find(0) != st.end()) { cout << "Yes" << endl; return 0; }
+    for(int i = 0; i < n; ++i) {
+        if(st.find(dst.get(0, i)) != st.end()) { cout << "Yes" << endl; return 0; }
+    }
+    cout << "No" << endl;
+}
