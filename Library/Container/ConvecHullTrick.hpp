2bd4616fc4d4427e93255d3ff2308aeee201e5d7
mdonaka <tmdonkt042+eg@gmail.com>
2023-03-05 02:37:46 +0900

    Add Convex Hull Tric Order

---
+++ Library/Container/ConvecHullTrick.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <deque>
+
+class ConvexHullTrickOrder {
+    using T = long long;
+    std::deque<std::pair<T, T>> lines;
+
+    static auto func(const std::pair<T, T>& line, const T& x) {
+        return x * line.first + line.second;
+    }
+    static auto check(const std::pair<T, T>& line1, const std::pair<T, T>& line2, const std::pair<T, T>& line3) {
+        auto [a1, b1] = line1;
+        auto [a2, b2] = line2;
+        auto [a3, b3] = line3;
+        return (a2 - a1) * (b3 - b2) >= (b2 - b1) * (a3 - a2);
+    }
+
+public:
+    ConvexHullTrickOrder() {}
+
+    auto add(const std::pair<T, T>& line) {
+        while(lines.size() > 1 &&
+              check(*std::next(lines.rbegin()), *lines.rbegin(), line)) {
+            lines.pop_back();
+        }
+        lines.emplace_back(line);
+    }
+    auto add(const T& a, const T& b) { add({a,b}); }
+
+    auto query(const T& x) {
+        while(lines.size() > 1 &&
+              func(*lines.begin(), x) > func(*std::next(lines.begin()), x)) {
+            lines.pop_front();
+        }
+        return func(*lines.begin(), x);
+    }
+};
