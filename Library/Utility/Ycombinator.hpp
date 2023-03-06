5a81fff4787b052bbbb8310d432dba9f4f139583
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2023-03-07 07:54:07 +0900

    fmt Ycombinator

--- Library/Utility/Ycombinator.hpp
+++ Library/Utility/Ycombinator.hpp
@@ -1,8 +1,8 @@
 
 auto Y = [](auto f) {
-	return [=](auto&&... args) {
-			return f(f, std::forward<decltype(args)>(args)...);
-		};
+    return [=](auto&&... args) {
+        return f(f, std::forward<decltype(args)>(args)...);
+    };
 };
 
 
