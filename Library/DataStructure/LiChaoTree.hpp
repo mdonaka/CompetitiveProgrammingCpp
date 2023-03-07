a6a3a835ad0504b7de3da2d9c2bac449fb6edf99
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2023-03-07 14:07:37 +0900

    fix li chao tree

--- Library/DataStructure/LiChaoTree.hpp
+++ Library/DataStructure/LiChaoTree.hpp
@@ -7,8 +7,7 @@
 class LiChaoTree {
     using T = long long;
     using Line = std::pair<T, T>;
-    constexpr static T mul_mx = 1e9;
-    constexpr static T add_mx = 1e18;
+    constexpr static T INF = std::numeric_limits< T >::max() / 2;
 
     int m_size;
     std::vector<T> m_x;
@@ -32,29 +31,28 @@ class LiChaoTree {
     }
 
 public:
-    LiChaoTree(const std::vector<T>& x_) {
+    LiChaoTree(const std::vector<T>& x_) :m_size(calcSize(x_.size())) {
         auto x = x_;
         std::sort(x.begin(), x.end());
         x.erase(std::unique(x.begin(), x.end()), x.end());
-
-        m_size = calcSize(x.size());
-        m_x = decltype(m_x)(m_size, mul_mx);
-        m_node = decltype(m_node)((m_size << 1) - 1, {0,add_mx});
+        m_x = decltype(m_x)(m_size);
         for(size_t i = 0; i < x.size(); ++i) {
             m_x[i] = x[i];
             m_xtoi.emplace(x[i], i);
         }
+        for(size_t i = x.size(); i < m_size; ++i) { m_x[i] = m_x[i - 1] + 1; }
+        m_node = decltype(m_node)(m_size << 1, {0,INF});
     }
 
     auto add(const Line& line) { add(line, 0, 0, m_size); }
     auto add(const T& a, const T& b) { add({a,b}); }
 
     auto query(const T& x) {
-        auto k = m_xtoi[x] + m_size - 1;
-        auto ret = add_mx << 1;
-        while(k >= 0) {
-            ret = std::min(ret, f(m_node[k], x));
-            k = (k - 1) >> 1;
+        auto k = m_xtoi[x] + m_size;
+        auto ret = INF;
+        while(k > 0) {
+            ret = std::min(ret, f(m_node[k - 1], x));
+            k >>= 1;
         }
         return ret;
     }
