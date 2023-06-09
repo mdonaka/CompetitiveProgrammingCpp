660ff08dc5dcf8ded9aecd15a8059ce1b26e191c
mdonaka <tmdonkt042+eg@gmail.com>
2023-06-09 22:55:59 +0900

    clean debug.hpp

--- Utils/debug.hpp
+++ Utils/debug.hpp
@@ -16,68 +16,68 @@ template<class T>constexpr inline auto d_val(T a, T b) { return b; }
 // 出力できる種類の追加
 template<class S, class T>
 std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {
-	os << "(" << p.first << ", " << p.second << ")";
-	return os;
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
 }
 // split
 inline auto split(std::string_view str, char del = ' ') {
-	std::list<std::string_view> sList;
-	int from = -1;
-	for (int i = 0; auto && c : str) {
-		if (c == ' ') {
-			sList.emplace_back(str.substr(from + 1, i - from - 2));
-			from = i;
-		}
-		++i;
-	}
-	sList.emplace_back(str.substr(from + 1, str.size() - from));
-	return sList;
+    std::list<std::string_view> sList;
+    int from = -1;
+    for(int i = 0; auto && c : str) {
+        if(c == ' ') {
+            sList.emplace_back(str.substr(from + 1, i - from - 2));
+            from = i;
+        }
+        ++i;
+    }
+    sList.emplace_back(str.substr(from + 1, str.size() - from));
+    return sList;
 }
 // 制約
 template <class T>
 concept Container = requires(T x) {
-	x.begin();
+    x.begin();
 };
 
 template <class T>
 concept Printable = requires(T x) {
-	std::cerr << x;
+    std::cerr << x;
 };
 
 // 出力
 constexpr auto print(const auto&) {
-	std::cerr << "<ERROR!> \"print\" of This type is not defined." << '\n';
+    std::cerr << "<ERROR!> \"print\" of This type is not defined." << '\n';
 }
 inline auto print(const std::string& s) { std::cerr << s << ' '; }
 constexpr auto print(const Printable auto& p) { std::cerr << p << ' '; }
 constexpr auto print(const Container auto& c) {
-	for (auto&& x : c) {
-		print(x);
-	}
-	std::cerr << '\n';
+    for(auto&& x : c) {
+        print(x);
+    }
+    std::cerr << '\n';
 }
 
 // 変数の出力
 constexpr auto printVariable(auto&& name, const auto& p) {
-	std::cerr << name << ": ";
-	print(p);
-	std::cerr << '\n';
+    std::cerr << name << ": ";
+    print(p);
+    std::cerr << '\n';
 }
 inline auto printVariable(auto&& name, const std::string& s) {
-	std::cerr << name << ": ";
-	print(s);
-	std::cerr << '\n';
+    std::cerr << name << ": ";
+    print(s);
+    std::cerr << '\n';
 }
 constexpr auto printVariable(auto&& name, const Container auto& c) {
-	std::cerr << "-- " << name << " --" << '\n';
-	print(c);
+    std::cerr << "-- " << name << " --" << '\n';
+    print(c);
 }
 
 // 1変数ずつ処理
 constexpr auto splitVariables(auto&& names) {}
 constexpr auto splitVariables(auto&& names, const auto& x,
-							  const auto&... tail) {
-	printVariable(names.front(), x);
-	names.pop_front();
-	splitVariables(std::forward<decltype(names)>(names), tail...);
+                              const auto&... tail) {
+    printVariable(names.front(), x);
+    names.pop_front();
+    splitVariables(std::forward<decltype(names)>(names), tail...);
 }
