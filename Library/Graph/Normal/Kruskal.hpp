fe09336c070e7bcb84cbd3824eae033bc09558c0
mdonaka <tmdonkt042+eg@gmail.com>
2023-06-14 04:14:29 +0900

    add kraskal test

---
+++ Library/Graph/Normal/Kruskal.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <queue>
+#include "./../Graph.hpp"
+#include "./../../DataStructure/UnionFind.hpp"
+
+template<class Node, class Cost>
+auto kruskal(const Graph<Node, Cost>& graph) {
+    auto n = graph.size();
+    auto min_spanning_tree = Graph(n);
+
+    auto dsu = UnionFind(n);
+    using Type = std::pair<Cost, std::pair<Node, Node>>;
+    std::priority_queue<Type, std::vector<Type>, std::greater<Type>> q;
+    for(const auto& [f, tc] : graph.getEdgesAll()) {
+        auto [t, c] = tc;
+        q.emplace(c, std::make_pair(f, t));
+    }
+
+    while(!q.empty()) {
+        auto [cost, ft] = q.top();
+        auto [from, to] = ft;
+        q.pop();
+        if(dsu.isSame(from, to)) { continue; }
+        dsu.unite(from, to);
+        min_spanning_tree.addEdgeUndirected(from, to, cost);
+    }
+
+    return min_spanning_tree;
+}
