df6f17337d76f55739f593a88b42c8f4a63b58ae
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2023-06-18 06:39:23 +0900

    update hld

--- Test/Graph/Tree/HeavyLightDecomposition_LCA.test.cpp
+++ Test/Graph/Tree/HeavyLightDecomposition_LCA.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_C"
 
 #include <iostream>
+#include "./../../../Library/Graph/Graph.hpp"
 #include "./../../../Library/Graph/Tree/HeavyLightDecomposition.hpp"
 
 using ll = long long;
@@ -11,19 +12,18 @@ constexpr char endl = '\n';
 signed main() {
     ll n;
     cin >> n;
-    std::unordered_multimap<int, int> tree;
+    auto tree = Graph<int, bool>(n);
     for(int f = 0; f < n; ++f) {
         ll k;
         cin >> k;
         for(int _ = 0; _ < k; ++_) {
             int t;
             cin >> t;
-            tree.emplace(f, t);
-            tree.emplace(t, f);
+            tree.addEdge(f, t);
         }
     }
 
-    auto hld = HeavyLightDecomposition(n, tree);
+    auto hld = HeavyLightDecomposition(tree);
 
     int q;
     cin >> q;
