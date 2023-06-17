0c45ae12fd3672192270d4a0ea68a6a36f8d8837
mdonaka <tmdonkt042+eg@gmail.com>
2023-06-18 06:04:34 +0900

    update lca test

--- Test/Graph/Tree/LowestCommonAncestor.test.cpp
+++ Test/Graph/Tree/LowestCommonAncestor.test.cpp
@@ -1,6 +1,8 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_C"
 
 #include <iostream>
+
+#include "./../../../Library/Graph/Graph.hpp"
 #include "./../../../Library/Graph/Tree/LowestCommonAncestor.hpp"
 
 using ll = long long;
@@ -11,19 +13,18 @@ constexpr char endl = '\n';
 signed main() {
     ll n;
     cin >> n;
-    std::unordered_multimap<int, int> tree;
+    Graph tree(n);
     for(int f = 0; f < n; ++f) {
         ll k;
         cin >> k;
         for(int _ = 0; _ < k; ++_) {
             int t;
             cin >> t;
-            tree.emplace(f, t);
-            tree.emplace(t, f);
+            tree.addEdgeUndirected(f, t);
         }
     }
 
-    auto lca = LowestCommonAncestor(n, tree, 0);
+    auto lca = LowestCommonAncestor(tree, 0);
 
     int q;
     cin >> q;
