059e5d96f9163e40b0af3c2ace5183b728bf2dac
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2023-06-18 07:07:19 +0900

    update flow

--- Test/Graph/Flow/Dinic.test.cpp
+++ Test/Graph/Flow/Dinic.test.cpp
@@ -1,27 +1,26 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/6/GRL_6_A"
 
 #include <iostream>
+#include "./../../../Library/Graph/Graph.hpp"
 #include "./../../../Library/Graph/Flow/Dinic.hpp"
 
 using ll = long long;
 using std::cout;
 using std::cin;
 constexpr char endl = '\n';
-template<class T, class S = T>
-using P = std::pair<T, S>;
 
 
 signed main() {
     int n, m;
     cin >> n >> m;
-    std::unordered_multimap<int, P<int, ll>> graph;
+    auto graph = Graph(n);
     for(int i = 0; i < m; ++i) {
         int u, v, c;
         cin >> u >> v >> c;
-        graph.emplace(u, P<int>{v, c});
+        graph.addEdge(u, v, c);
     }
 
-    auto mf = Dinic(n, graph);
+    auto mf = Dinic(graph);
 
     cout << mf.max_flow(0, n - 1) << endl;
 }
