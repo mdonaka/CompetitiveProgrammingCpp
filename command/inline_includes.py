603eda8add6ff8f543c9735371d8931f9ebb1cc0
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2024-07-29 03:10:25 +0900

    update include deploy

--- command/inline_includes.py
+++ command/inline_includes.py
@@ -1,5 +1,8 @@
+import dataclasses
 import sys
+from collections import deque
 from pathlib import Path
+from typing import Callable, Optional
 
 
 def open_src(filepath: Path) -> list[str]:
@@ -7,11 +10,69 @@ def open_src(filepath: Path) -> list[str]:
         return [row for row in f]
 
 
-def replace(root: Path, line: str) -> str:
-    if "#include" not in line:
-        return line
-    filepath = root / line.split('"')[1]
+@dataclasses.dataclass
+class Graph:
+    size: int = 0
+    itop: list[Path] = dataclasses.field(default_factory=list)
+    ptoi: dict[Path, int] = dataclasses.field(default_factory=dict)
+    edge: list[list[int]] = dataclasses.field(default_factory=list)
 
+    def __add_node(self, u: Path) -> None:
+        if u not in self.ptoi:
+            self.itop.append(u)
+            self.ptoi[u] = self.size
+            self.edge.append([])
+            self.size += 1
+
+    def add_edge(self, u: Path, v: Path) -> None:
+        self.__add_node(u)
+        self.__add_node(v)
+        self.edge[self.ptoi[u]].append(self.ptoi[v])
+
+    def topological_lambda(self, f: Callable[[Path], None], *refs) -> None:
+        use = [0 for _ in range(self.size)]
+        for u in range(self.size):
+            for v in self.edge[u]:
+                use[v] += 1
+
+        queue: deque = deque()
+        for u, c in enumerate(use):
+            if c == 0:
+                queue.append(u)
+
+        while queue:
+            u = queue.popleft()
+            f(self.itop[u], *refs)
+            for v in self.edge[u]:
+                use[v] -= 1
+                if use[v] == 0:
+                    queue.append(v)
+
+
+@dataclasses.dataclass
+class Source:
+    filepath: Path
+    includes: list[Path] = dataclasses.field(default_factory=list, init=False)
+
+    def __post_init__(self) -> None:
+        root = self.filepath.parent
+        for line in open_src(self.filepath):
+            if "#include" in line and "Utils/" not in line:
+                sp = line.split('"')
+                if len(sp) > 1:
+                    filepath = root / sp[1]
+                    self.includes.append(filepath.resolve())
+
+    def get_includes_graph(self, graph_: Optional[Graph] = None) -> Graph:
+        graph = graph_ if graph_ else Graph()
+        for include_path in self.includes:
+            graph.add_edge(include_path, self.filepath)
+            src = Source(include_path)
+            src.get_includes_graph(graph)
+        return graph
+
+
+def replace(filepath: Path) -> str:
     exclude_words = [
         "#pragma once",
         "#include",
@@ -28,14 +89,25 @@ if __name__ == "__main__":
     if len(args) != 2:
         raise RuntimeError("USAGE: python {this file} {src file}")
 
-    filepath = Path(args[1])
-    root = filepath.parent
+    filepath = Path(args[1]).resolve()
 
-    rep = False
-    ret = ""
+    source = Source(filepath)
+    graph = source.get_includes_graph()
+
+    flag = 0
+    source_lst: list[str] = []
     for line in open_src(filepath):
         if "tag:includes begin" in line or "tag:includes end" in line:
-            rep ^= True
-        ret += line if not rep else replace(root, line)
+            flag += 1
+        elif flag == 1:
 
-    print(ret)
+            def f(filepath: Path, replace_source: list):
+                if "main.cpp" not in str(filepath):
+                    replace_source.append(replace(filepath))
+
+            graph.topological_lambda(lambda filepath: f(filepath, source_lst))
+            flag += 1
+        elif flag == 0 or flag == 3:
+            source_lst.append(line)
+
+    print("".join(source_lst))
