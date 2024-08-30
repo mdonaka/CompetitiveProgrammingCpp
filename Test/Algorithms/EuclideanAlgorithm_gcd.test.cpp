22d5923a2f5704225555e28fe29ae191bfc34339
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2024-08-30 15:28:21 +0900

    add gcd test

--- Test/Algorithms/EuclideanAlgorithm_gcd.test.cpp
+++ Test/Algorithms/EuclideanAlgorithm_gcd.test.cpp
@@ -1,18 +1,17 @@
-// TODO: いい問題を探す
-#define PROBLEM ""
-#define IGNORE
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/challenges/search/titles/1009"
+
+// tag:includes begin
+#include "../../Library/Range/istream.hpp"
+#include "./../../Library/Algorithms/EuclideanAlgorithm.hpp"
+// tag:includes end
 
 #include <iostream>
 
-using ll = long long;
-using std::cin;
-using std::cout;
-constexpr char endl = '\n';
-struct Preprocessing {
-  Preprocessing() {
-    std::cin.tie(0);
-    std::ios::sync_with_stdio(0);
-  };
-} _Preprocessing;
-
-signed main() {}
+signed main() {
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(0);
+  for (auto [x, y] : mtd::views::istream<int, int>()) {
+    auto ea = EuclideanAlgorithm(x, y);
+    std::cout << ea.gcd() << std::endl;
+  }
+}
