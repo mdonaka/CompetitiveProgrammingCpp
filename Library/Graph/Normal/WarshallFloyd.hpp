5eee21bf782c8fe38da2f2ee09e9e6b5123514ba
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2024-08-31 21:47:19 +0900

    fix warshallfloyd

--- Library/Graph/Normal/WarshallFloyd.hpp
+++ Library/Graph/Normal/WarshallFloyd.hpp
@@ -10,10 +10,12 @@ auto warshallFloyd(int n, const Graph<Node, Cost>& graph,
   std::vector<std::vector<Cost>> cost(n, std::vector<Cost>(n, lim));
   for (const auto& [from, to, c] : graph.getEdges()) { cost[from][to] = c; }
   for (int i = 0; i < n; ++i) { cost[i][i] = 0; }
-  for (int k = 0; k < n; ++k)
-    for (int f = 0; f < n; ++f)
+  for (int k = 0; k < n; ++k) {
+    for (int f = 0; f < n; ++f) {
       for (int t = 0; t < n; ++t) {
         cost[f][t] = std::min(cost[f][t], cost[f][k] + cost[k][t]);
       }
+    }
+  }
   return cost;
 }
