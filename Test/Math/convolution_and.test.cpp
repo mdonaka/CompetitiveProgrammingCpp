936d1741b76dc7e9e57039ffe10c03e7f7e771bc
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2024-11-06 16:42:01 +0900

    add modint

--- Test/Math/convolution_and.test.cpp
+++ Test/Math/convolution_and.test.cpp
@@ -3,17 +3,19 @@
 
 // begin:tag includes
 #include "../../Library/Math/Convolution.hpp"
+#include "../../Library/Math/Modint.hpp"
 #include "../../Library/Range/istream.hpp"
 // end:tag includes
-#include "../../Library/Debug/Dump.hpp"
 
 int main() {
   std::cin.tie(0);
   std::ios::sync_with_stdio(0);
 
+  using mint = mtd::ModInt<998244353>;
+
   auto [n] = mtd::io::in<int>();
-  auto a = mtd::io::_input<mtd::io::type::vec<int>>(1LL << n);
-  auto b = mtd::io::_input<mtd::io::type::vec<int>>(1LL << n);
+  auto a = mtd::io::_input<mtd::io::type::vec<mint>>(1LL << n);
+  auto b = mtd::io::_input<mtd::io::type::vec<mint>>(1LL << n);
 
   auto ans = mtd::convolution::bitwise_and(a, b);
   for (auto x : ans) { std::cout << x << " "; }
