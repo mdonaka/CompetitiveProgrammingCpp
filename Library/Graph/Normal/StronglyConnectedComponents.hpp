20a4953edbf1c7eda2880ba00f51fc184bda0b1f
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2024-11-16 03:31:01 +0900

    update scc to fast

--- Library/Graph/Normal/StronglyConnectedComponents.hpp
+++ Library/Graph/Normal/StronglyConnectedComponents.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <ranges>
 #include <unordered_set>
 #include <vector>
 
@@ -25,48 +26,56 @@ namespace mtd {
     const std::vector<int> m_group;
 
     template <class F>
-    static inline auto dfs(const Graph<Node, Cost>& graph, int from,
-                           std::vector<bool>& is_used, const F& f) -> void {
+    constexpr static inline auto dfs(const Graph<Node, Cost>& graph, int from,
+                                     std::vector<bool>& is_used, const F& f)
+        -> void {
+      std::vector<Node> stk{from};
       is_used[from] = true;
-      for (const auto& [to, _] : graph.getEdges(from)) {
-        if (is_used[to]) { continue; }
-        dfs(graph, to, is_used, f);
+      while (!stk.empty()) {
+        auto fr = stk.back();
+        stk.pop_back();
+        f(fr);
+        for (const auto& [to, _] : graph.getEdges(fr)) {
+          if (is_used[to]) { continue; }
+          is_used[to] = true;
+          stk.emplace_back(to);
+        }
       }
-      f(from);
     }
 
-    static inline auto constructGroup(const Graph<Node, Cost>& graph) {
+    constexpr static auto constructGroup(const Graph<Node, Cost>& graph) {
       int n = graph.size();
-      std::vector<int> order;
-      order.reserve(n);
+      std::vector<Node> order;
       std::vector<bool> is_used(n);
-      for (int from = 0; from < n; ++from)
-        if (!is_used[from]) {
-          dfs(graph, from, is_used, [&](int f) { order.emplace_back(f); });
-        }
+      for (auto from : std::views::iota(0, n)) {
+        if (is_used[from]) { continue; }
+        dfs(graph, from, is_used, [&](int f) { order.emplace_back(f); });
+      }
 
       int g = 0;
-      std::vector<int> group(n);
+      std::vector<Node> group(n);
       std::vector<bool> is_used2(n);
       auto rev = graph.reverse();
-      for (int i = n - 1; i >= 0; --i)
-        if (!is_used2[order[i]]) {
-          dfs(rev, order[i], is_used2, [&](int f) { group[f] = g; });
-          ++g;
-        }
+      for (auto from : order) {
+        if (is_used2[from]) { continue; }
+        dfs(rev, from, is_used2, [&](int f) { group[f] = g; });
+        ++g;
+      }
       return group;
     }
-    auto constructGroupNodes() const {}
 
   public:
-    [[deprecated]] StronglyConnectedComponents(const Graph<Node, Cost>& graph)
+    [[deprecated]] constexpr StronglyConnectedComponents(
+        const Graph<Node, Cost>& graph)
         : m_graph(graph), m_group(constructGroup(m_graph)) {}
     // graphのコピーコストが大きいのでこっち推奨
-    StronglyConnectedComponents(Graph<Node, Cost>&& graph)
+    constexpr StronglyConnectedComponents(Graph<Node, Cost>&& graph)
         : m_graph(std::move(graph)), m_group(constructGroup(m_graph)) {}
 
-    auto isSameGroup(int a, int b) const { return m_group[a] == m_group[b]; }
-    auto getGroupNodes() const {
+    constexpr auto isSameGroup(int a, int b) const {
+      return m_group[a] == m_group[b];
+    }
+    constexpr auto getGroupNodes() const {
       auto size = *std::max_element(m_group.begin(), m_group.end()) + 1;
       std::vector<std::vector<int>> groupNodes(size);
       for (int gi = 0; gi < m_graph.size(); ++gi) {
@@ -74,12 +83,14 @@ namespace mtd {
       }
       return groupNodes;
     }
-    auto getGroupGraph() const {
+    constexpr auto getGroupGraph() const {
       std::unordered_set<std::pair<int, int>, HashPair> st;
       st.reserve(m_graph.size());
-      for (int f = 0; f < m_graph.size(); ++f)
-        for (const auto& [t, _] : m_graph.getEdges(f))
+      for (int f = 0; f < m_graph.size(); ++f) {
+        for (const auto& [t, _] : m_graph.getEdges(f)) {
           if (!isSameGroup(f, t)) { st.emplace(m_group[f], m_group[t]); }
+        }
+      }
       Graph<Node, Cost> ret(m_graph.size());
       for (const auto& [f, t] : st) { ret.addEdge(f, t); }
       return ret;
