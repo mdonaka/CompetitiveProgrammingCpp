6a7c6c7d14ab95c5dd25a311dde1e3d666607e5f
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2024-12-11 01:15:34 +0900

    fix segtree tests

--- Test/DataStructure/DynamicSegmentTree.test.cpp
+++ Test/DataStructure/DynamicSegmentTree.test.cpp
@@ -1,45 +1,46 @@
 #define PROBLEM "https://yukicoder.me/problems/no/1282"
 
-#include "./../../Library/DataStructure/DynamicSegmentTree.hpp"
-
 #include <algorithm>
 #include <iostream>
+#include <ranges>
 #include <vector>
 
-using ll = long long;
-using std::cin;
-using std::cout;
-constexpr char endl = '\n';
+// begin:tag includes
+#include "./../../Library/DataStructure/DynamicSegmentTree.hpp"
+// end:tag includes
 
-struct Functor {
-  auto operator()(ll a, ll b) const { return a + b; }
-};
-using M = mtd::Monoid<ll, 0, Functor>;
+using ll = long long;
 
 signed main() {
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(0);
+
   ll n;
-  cin >> n;
+  std::cin >> n;
   std::vector<ll> a, b;
   a.reserve(n);
   b.reserve(n);
   for (int i = 0; i < n; ++i) {
     ll x;
-    cin >> x;
+    std::cin >> x;
     a.emplace_back(x);
   }
   for (int i = 0; i < n; ++i) {
     ll x;
-    cin >> x;
+    std::cin >> x;
     b.emplace_back(x);
   }
-  std::sort(a.begin(), a.end());
+  std::ranges::sort(a);
+
+  auto op = [](ll a, ll b) { return a + b; };
+  using M = mtd::Monoid<ll, 0, decltype(op)>;
+  auto segtree = mtd::DynamicSegmentTree<M>();
 
   ll ans = 0;
-  auto segtree = mtd::DynamicSegmentTree<M>();
   for (int i = 0; i < n; ++i) {
     segtree.add(b[i], 1);
     ans += segtree.query(0, a[i] - 1);
   }
 
-  cout << ans << endl;
+  std::cout << ans << std::endl;
 }
--- Test/DataStructure/SegmentTree_RSQ.test.cpp
+++ Test/DataStructure/SegmentTree_RSQ.test.cpp
@@ -3,30 +3,30 @@
 
 #include <iostream>
 
+// begin:tag includes
 #include "./../../Library/DataStructure/SegmentTree.hpp"
+// end:tag includes
 
 using ll = long long;
-using std::cin;
-using std::cout;
-constexpr char endl = '\n';
 
 signed main() {
-  int n, q;
-  cin >> n >> q;
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(0);
 
-  struct F {
-    auto operator()(ll a, ll b) const { return a + b; }
-  };
-  using M = mtd::Monoid<ll, 0, F>;
+  int n, q;
+  std::cin >> n >> q;
+
+  auto op = [](ll a, ll b) { return a + b; };
+  using M = mtd::Monoid<ll, 0, decltype(op)>;
   auto segtree = mtd::SegmentTree<M>(n);
 
   for (int _ = 0; _ < q; ++_) {
     int k, x, y;
-    cin >> k >> x >> y;
+    std::cin >> k >> x >> y;
     if (k == 0) {
       segtree.add(x - 1, y);
     } else {
-      cout << segtree.query(x - 1, y - 1) << endl;
+      std::cout << segtree.query(x - 1, y - 1) << std::endl;
     }
   }
 }
