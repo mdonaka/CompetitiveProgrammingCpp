38d57bf48f836048f606161ab83d4b8342d30023
mdonaka <tmdonkt042+eg@gmail.com>
2024-12-11 01:15:34 +0900

    fix segtree tests

--- Library/DataStructure/DynamicSegmentTree.hpp
+++ Library/DataStructure/DynamicSegmentTree.hpp
@@ -6,30 +6,21 @@
 #include <utility>
 #include <vector>
 
+#include "../Algebraic/Monoid.hpp"
+
 namespace mtd {
-  template <class T>
-  class isMonoid {
-    template <class U>
-    static auto check(U x) -> decltype(x.binaryOperation(x), std::true_type{});
-    static std::false_type check(...);
-
-  public:
-    static bool const value = decltype(check(std::declval<T>()))::value;
-  };
-
-  template <class Monoid, int size = static_cast<int>(1e9 + 1),
-            std::enable_if_t<isMonoid<Monoid>::value, std::nullptr_t> = nullptr>
+  template <monoid Monoid, int size = static_cast<int>(1e9 + 1)>
   class DynamicSegmentTree {
   private:
     std::unordered_map<int, Monoid> m_node;
     using S = decltype(Monoid().m_val);
 
-    auto _get(int i) const {
+    constexpr auto _get(int i) const {
       return (m_node.find(i) == m_node.end()) ? Monoid() : m_node.at(i);
     }
 
     template <class Lambda>
-    auto _update_op(int itr, Monoid&& val, const Lambda& op) {
+    constexpr auto _update_op(int itr, Monoid&& val, const Lambda& op) {
       int i = itr + size - 1;
       m_node[i] = op(_get(i), std::forward<decltype(val)>(val));
       while (i) {
@@ -38,7 +29,7 @@ namespace mtd {
       }
     }
 
-    auto _query(int _l, int _r) const {
+    constexpr auto _query(int _l, int _r) const {
       _l = std::max(_l, 0);
       _r = std::min(_r, size - 1);
       auto l = _l + size;
@@ -59,7 +50,7 @@ namespace mtd {
       return lm.binaryOperation(rm);
     }
 
-    auto _construct(const std::vector<S>& vec) {
+    constexpr auto _construct(const std::vector<S>& vec) {
       for (unsigned int i = 0; i < vec.size(); ++i) {
         m_node[i + size - 1] = Monoid(vec[i]);
       }
@@ -69,41 +60,24 @@ namespace mtd {
     }
 
   public:
-    DynamicSegmentTree() {}
+    constexpr DynamicSegmentTree() {}
 
     template <class Lambda>
-    auto update_op(int itr, Monoid&& val, const Lambda& op) {
+    constexpr auto update_op(int itr, Monoid&& val, const Lambda& op) {
       return _update_op(itr, std::forward<Monoid>(val), op);
     }
-    auto update(int itr, Monoid&& val) {
+    constexpr auto update(int itr, Monoid&& val) {
       return update_op(itr, std::forward<Monoid>(val),
                        [](const Monoid&, const Monoid& m2) { return m2; });
     }
-    auto add(int itr, Monoid&& val) {
+    constexpr auto add(int itr, Monoid&& val) {
       return update_op(itr, std::forward<Monoid>(val),
                        [](const Monoid& m1, const Monoid& m2) {
                          return Monoid(m1.m_val + m2.m_val);
                        });
     }
-    auto query(int l, int r) const { return _query(l, r).m_val; }
-    auto query_all() const { return m_node[0].m_val; }
+    constexpr auto query(int l, int r) const { return _query(l, r).m_val; }
+    constexpr auto query_all() const { return m_node[0].m_val; }
   };
 
-  template <class S,    // 要素の型
-            S element,  // 元
-            class T  // lambdaはC++20じゃないと渡せなかった．．．
-                     // S T(S, S)  // 2項演算子
-            >
-  struct Monoid {
-    S m_val;
-    Monoid() : m_val(element) {}
-    Monoid(S val) : m_val(val) {}
-    Monoid binaryOperation(const Monoid& m2) const {
-      return T()(m_val, m2.m_val);
-    }
-    friend std::ostream& operator<<(std::ostream& os,
-                                    const Monoid<S, element, T>& m) {
-      return os << m.m_val;
-    }
-  };
 }  // namespace mtd
--- Test/DataStructure/DynamicSegmentTree_RMQ.test.cpp
+++ Test/DataStructure/DynamicSegmentTree_RMQ.test.cpp
@@ -3,30 +3,30 @@
 
 #include <iostream>
 
+// begin:tag includes
 #include "./../../Library/DataStructure/DynamicSegmentTree.hpp"
+// end:tag includes
 
 using ll = long long;
-using std::cin;
-using std::cout;
-constexpr char endl = '\n';
 
 signed main() {
-  int n, q;
-  cin >> n >> q;
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(0);
 
-  struct F {
-    auto operator()(ll a, ll b) const { return std::min(a, b); }
-  };
-  using M = mtd::Monoid<ll, (1LL << 31) - 1, F>;
+  int n, q;
+  std::cin >> n >> q;
+
+  auto op = [](ll a, ll b) { return std::min(a, b); };
+  using M = mtd::Monoid<ll, (1LL << 31) - 1, decltype(op)>;
   auto segtree = mtd::DynamicSegmentTree<M>();
 
   for (int _ = 0; _ < q; ++_) {
     int k, x, y;
-    cin >> k >> x >> y;
+    std::cin >> k >> x >> y;
     if (k == 0) {
       segtree.update(x, y);
     } else {
-      cout << segtree.query(x, y) << endl;
+      std::cout << segtree.query(x, y) << std::endl;
     }
   }
 }
