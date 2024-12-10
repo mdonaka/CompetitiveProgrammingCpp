4f09968de7dd8d47a29578eb5d088fedb53daebc
mdonaka <tmdonkt042+eg@gmail.com>
2024-12-11 00:59:34 +0900

    update monoid class

---
+++ Library/Algebraic/Monoid.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+namespace mtd {
+
+  template <class S,    // set
+            S element,  // identity element
+            class op    // binary operation
+            >
+  requires std::is_invocable_r_v<S, op, S, S>
+  struct Monoid {
+    using value_type = S;
+    constexpr static S _element = element;
+    using op_type = op;
+
+    S m_val;
+    constexpr Monoid(S val) : m_val(val) {}
+    constexpr Monoid() : Monoid(element) {}
+    constexpr Monoid binaryOperation(const Monoid& m2) const {
+      return op()(m_val, m2.m_val);
+    }
+    friend std::ostream& operator<<(std::ostream& os,
+                                    const Monoid<S, element, op>& m) {
+      return os << m.m_val;
+    }
+  };
+
+  namespace __detail {
+    template <typename T, template <typename, auto, typename> typename S>
+    concept is_specialization_of = requires {
+      typename std::enable_if_t<std::is_same_v<
+          T, S<typename T::value_type, T::_element, typename T::op_type>>>;
+    };
+  }  // namespace __detail
+
+  template <typename M>
+  concept monoid = __detail::is_specialization_of<M, Monoid>;
+
+}  // namespace mtd
--- Library/DataStructure/SegmentTree.hpp
+++ Library/DataStructure/SegmentTree.hpp
@@ -1,36 +1,29 @@
 #pragma once
 
 #include <deque>
+#include <iostream>
 #include <utility>
 #include <vector>
 
+#include "../Algebraic/Monoid.hpp"
+
 namespace mtd {
-  template <class T>
-  class isMonoid {
-    template <class U>
-    static auto check(U x) -> decltype(x.binaryOperation(x), std::true_type{});
-    static std::false_type check(...);
 
-  public:
-    static bool const value = decltype(check(std::declval<T>()))::value;
-  };
-
-  template <class Monoid,
-            std::enable_if_t<isMonoid<Monoid>::value, std::nullptr_t> = nullptr>
+  template <monoid Monoid>
   class SegmentTree {
   private:
     const int m_size;
     std::vector<Monoid> m_node;
     using S = decltype(Monoid().m_val);
 
-    int calcSize(int n) const {
+    constexpr int calcSize(int n) const {
       int size = 1;
       while (size < n) { size <<= 1; }
       return size;
     }
 
     template <class Lambda>
-    auto _update_op(int itr, Monoid&& val, const Lambda& op) {
+    constexpr auto _update_op(int itr, Monoid&& val, const Lambda& op) {
       int i = itr + m_size - 1;
       m_node[i] = op(m_node[i], std::forward<decltype(val)>(val));
       while (i) {
@@ -39,11 +32,11 @@ namespace mtd {
       }
     }
 
-    auto _query(int _l, int _r) const {
+    constexpr auto _query(int _l, int _r) const {
       _l = std::max(_l, 0);
       _r = std::min(_r, m_size - 1);
       auto l = _l + m_size;
-      int r = _r + m_size;
+      auto r = _r + m_size;
       auto lm = Monoid();
       auto rm = Monoid();
       while (l <= r) {
@@ -60,7 +53,7 @@ namespace mtd {
       return lm.binaryOperation(rm);
     }
 
-    auto _construct(const std::vector<S>& vec) {
+    constexpr auto _construct(const std::vector<S>& vec) {
       for (unsigned int i = 0; i < vec.size(); ++i) {
         m_node[i + m_size - 1] = Monoid(vec[i]);
       }
@@ -76,23 +69,23 @@ namespace mtd {
     }
 
     template <class Lambda>
-    auto update_op(int itr, Monoid&& val, const Lambda& op) {
+    constexpr auto update_op(int itr, Monoid&& val, const Lambda& op) {
       return _update_op(itr, std::forward<Monoid>(val), op);
     }
-    auto update(int itr, Monoid&& val) {
+    constexpr auto update(int itr, Monoid&& val) {
       return update_op(itr, std::forward<Monoid>(val),
                        [](const Monoid&, const Monoid& m2) { return m2; });
     }
-    auto add(int itr, Monoid&& val) {
+    constexpr auto add(int itr, Monoid&& val) {
       return update_op(itr, std::forward<Monoid>(val),
                        [](const Monoid& m1, const Monoid& m2) {
                          return Monoid(m1.m_val + m2.m_val);
                        });
     }
-    auto query(int l, int r) const { return _query(l, r).m_val; }
-    auto query_all() const { return m_node[0].m_val; }
+    constexpr auto query(int l, int r) const { return _query(l, r).m_val; }
+    constexpr auto query_all() const { return m_node[0].m_val; }
 
-    auto output() const {
+    constexpr auto debug() const {
       for (int i = 0; i < m_size; ++i) {
         std::cout << m_node[m_size + i - 1] << " ";
       }
@@ -100,21 +93,4 @@ namespace mtd {
     }
   };
 
-  template <class S,    // 要素の型
-            S element,  // 元
-            class T  // lambdaはC++20じゃないと渡せなかった．．．
-                     // S T(S, S)  // 2項演算子
-            >
-  struct Monoid {
-    S m_val;
-    Monoid() : m_val(element) {}
-    Monoid(S val) : m_val(val) {}
-    Monoid binaryOperation(const Monoid& m2) const {
-      return T()(m_val, m2.m_val);
-    }
-    friend std::ostream& operator<<(std::ostream& os,
-                                    const Monoid<S, element, T>& m) {
-      return os << m.m_val;
-    }
-  };
 }  // namespace mtd
