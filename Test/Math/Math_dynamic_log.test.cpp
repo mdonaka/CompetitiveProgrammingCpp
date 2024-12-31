a117e4dfb29f36dcd1879ce4360e450fa9c9aee9
mdonaka <tmdonkt042+eg@gmail.com>
2025-01-01 05:22:02 +0900

    remove debug comment

--- Test/Math/Math_dynamic_log.test.cpp
+++ Test/Math/Math_dynamic_log.test.cpp
@@ -2,8 +2,6 @@
 
 #include <ranges>
 
-#include "./../../Library/Debug/Dump.hpp"
-
 // begin:tag includes
 #include "../../Library/Math/DynamicModInt.hpp"
 // end:tag includes
@@ -16,14 +14,9 @@ signed main() {
 
   int t;
   std::cin >> t;
-  // t = 1;
   for ([[maybe_unused]] auto _ : std::views::iota(0, t)) {
     ll x, y, m;
     std::cin >> x >> y >> m;
-
-    // x = 156648746;
-    // y = 154396384;
-    // m = 916122984;
 
     using mint = mtd::DynamicModInt<>;
     mint::set_mod(m);
@@ -31,8 +24,6 @@ signed main() {
 
     if (auto ans = math.log(x, y); ans) {
       std::cout << ans.value() << std::endl;
-      // dump(math.pow(x, 1618984));
-      // dump(math.pow(x, ans.value()));
     } else {
       std::cout << -1 << std::endl;
     }
