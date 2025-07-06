38a2a52e051f07a53645a4a641e10fb991dd3e84
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2025-07-06 21:41:14 +0900

    add constexpr to io

--- Library/Utility/io.hpp
+++ Library/Utility/io.hpp
@@ -20,20 +20,20 @@ namespace mtd {
     }  // namespace __details
 
     template <class T>
-    auto _input() {
+    constexpr auto _input() {
       T x;
       std::cin >> x;
       return x;
     }
     template <typename T>
     requires requires { typename std::tuple_size<T>::type; }
-    auto _input() {
+    constexpr auto _input() {
       T x;
       util::tuple_for_each([](auto&& i) { std::cin >> i; }, x);
       return x;
     }
     template <__details::is_vec T>
-    auto _input(int n) {
+    constexpr auto _input(int n) {
       std::vector<typename T::value_type> v;
       v.reserve(n);
       for (auto i : std::views::iota(0, n)) {
@@ -42,7 +42,7 @@ namespace mtd {
       return v;
     }
     template <__details::is_mat T>
-    auto _input(int h, int w) {
+    constexpr auto _input(int h, int w) {
       T mat;
       mat.reserve(h);
       for (auto i : std::views::iota(0, h)) {
@@ -52,16 +52,17 @@ namespace mtd {
     }
 
     template <int N, class Tuple, class T, class... Args, class... Sizes>
-    auto _tuple_input(Tuple& t, Sizes... sizes);
+    constexpr auto _tuple_input(Tuple& t, Sizes... sizes);
     template <int N, class Tuple, __details::is_vec T, class... Args,
               class Size, class... Sizes>
-    auto _tuple_input(Tuple& t, Size size, Sizes... sizes);
+    constexpr auto _tuple_input(Tuple& t, Size size, Sizes... sizes);
     template <int N, class Tuple, __details::is_mat T, class... Args,
               class Size, class... Sizes>
-    auto _tuple_input(Tuple& t, Size size_h, Size size_w, Sizes... sizes);
+    constexpr auto _tuple_input(Tuple& t, Size size_h, Size size_w,
+                                Sizes... sizes);
 
     template <int N, class Tuple, class T, class... Args, class... Sizes>
-    auto _tuple_input(Tuple& t, Sizes... sizes) {
+    constexpr auto _tuple_input(Tuple& t, Sizes... sizes) {
       std::get<N>(t) = _input<T>();
       if constexpr (sizeof...(Args) > 0) {
         _tuple_input<N + 1, Tuple, Args...>(t, sizes...);
@@ -69,7 +70,7 @@ namespace mtd {
     }
     template <int N, class Tuple, __details::is_vec T, class... Args,
               class Size, class... Sizes>
-    auto _tuple_input(Tuple& t, Size size, Sizes... sizes) {
+    constexpr auto _tuple_input(Tuple& t, Size size, Sizes... sizes) {
       std::get<N>(t) = _input<T>(size);
       if constexpr (sizeof...(Args) > 0) {
         _tuple_input<N + 1, Tuple, Args...>(t, sizes...);
@@ -77,7 +78,8 @@ namespace mtd {
     }
     template <int N, class Tuple, __details::is_mat T, class... Args,
               class Size, class... Sizes>
-    auto _tuple_input(Tuple& t, Size size_h, Size size_w, Sizes... sizes) {
+    constexpr auto _tuple_input(Tuple& t, Size size_h, Size size_w,
+                                Sizes... sizes) {
       std::get<N>(t) = _input<T>(size_h, size_w);
       if constexpr (sizeof...(Args) > 0) {
         _tuple_input<N + 1, Tuple, Args...>(t, sizes...);
@@ -85,7 +87,8 @@ namespace mtd {
     }
 
     template <class... Args, class... Sizes>
-    requires(std::convertible_to<Sizes, size_t>&&...) auto in(Sizes... sizes) {
+    requires(std::convertible_to<Sizes, size_t>&&...) constexpr auto in(
+        Sizes... sizes) {
       auto base = std::tuple<Args...>();
       _tuple_input<0, decltype(base), Args...>(base, sizes...);
       return base;
